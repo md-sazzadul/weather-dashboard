2a2ab44f1eda11d905232e473112223d8bb4b53e
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-02-14 13:17:28 +0100

    added local storage and made favourite toggle functional

--- src/components/Weather/AddToFavourite.jsx
+++ src/components/Weather/AddToFavourite.jsx
@@ -1,12 +1,27 @@
-import heart from "../../assets/heart.svg";
+import { useContext, useState } from "react";
+import RedHeartIcon from "../../assets/heart-red.svg";
+import HeartIcon from "../../assets/heart.svg";
+import { FavouriteContext } from "../../context";
 
 const AddToFavourite = () => {
+  const { addToFavourites, removeFromFavourites, favourites } =
+    useContext(FavouriteContext);
+
+  const [isFavourite, setIsFavourite] = useState(false);
+
+  const handleFavourites = () => {
+    setIsFavourite(!isFavourite);
+  };
+
   return (
     <div className="md:col-span-2">
       <div className="flex items-center justify-end space-x-6">
-        <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
+        <button
+          className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]"
+          onClick={handleFavourites}
+        >
           <span>Add to Favourite</span>
-          <img src={heart} alt="heart" />
+          <img src={isFavourite ? RedHeartIcon : HeartIcon} alt="heart" />
         </button>
       </div>
     </div>
--- src/components/Weather/WeatherBoard.jsx
+++ src/components/Weather/WeatherBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { WeatherContext } from "../../context";
 import AddToFavourite from "./AddToFavourite";
 import WeatherCondition from "./WeatherCondition";
