c010dcacf334981d61f1b1e231d6ec9bc2739696
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-02-14 15:50:40 +0100

    added backgrounds for different weathers

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,16 +1,69 @@
+import { useContext, useEffect, useState } from "react";
+import ClearSkyImage from "./assets/backgrounds/clear-sky.jpg";
+import FewCloudsImage from "./assets/backgrounds/few-clouds.jpg";
+import MistImage from "./assets/backgrounds/mist.jpeg";
+import RainyDayImage from "./assets/backgrounds/rainy-day.jpg";
+import ScatterdCloudsImage from "./assets/backgrounds/scattered-clouds.jpg";
+import SnowImage from "./assets/backgrounds/snow.jpg";
+import SunnyImage from "./assets/backgrounds/sunny.jpg";
+import ThunderStormImage from "./assets/backgrounds/thunderstorm.jpg";
+import WinterImage from "./assets/backgrounds/winter.jpg";
 import Header from "./components/Header/Header";
 import WeatherBoard from "./components/Weather/WeatherBoard";
+import { WeatherContext } from "./context";
 
 const Page = () => {
+  const { weatherData, loading } = useContext(WeatherContext);
+  const [climateImage, setClimateImage] = useState("");
+
+  function getBackgroundImage(climate) {
+    switch (climate) {
+      case "Rain":
+        return RainyDayImage;
+      case "Clouds":
+        return ScatterdCloudsImage;
+      case "Clear":
+        return ClearSkyImage;
+      case "Snow":
+        return SnowImage;
+      case "Thunder":
+        return ThunderStormImage;
+      case "Fog":
+        return WinterImage;
+      case "Haze":
+        return FewCloudsImage;
+      case "Mist":
+        return MistImage;
+      default:
+        return SunnyImage;
+    }
+  }
+
+  useEffect(() => {
+    const bgImage = getBackgroundImage(weatherData.climate);
+    setClimateImage(bgImage);
+  }, [weatherData.climate]);
+
   return (
-    <div className="grid place-items-center h-screen">
-      <Header></Header>
-      <main>
-        <section>
-          <WeatherBoard></WeatherBoard>
-        </section>
-      </main>
-    </div>
+    <>
+      {loading.state ? (
+        <div>
+          <p>{loading.message}</p>
+        </div>
+      ) : (
+        <div
+          style={{ backgroundImage: `url('${climateImage}')` }}
+          className="grid place-items-center h-screen bg-no-repeat bg-cover"
+        >
+          <Header></Header>
+          <main>
+            <section>
+              <WeatherBoard></WeatherBoard>
+            </section>
+          </main>
+        </div>
+      )}
+    </>
   );
 };
 
