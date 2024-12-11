dc0be9861ffaa54d9bc8032acf0545593a43449a
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-12-11 23:41:27 +0600

    added styling and aesthetics

--- src/components/Header/Favourite.jsx
+++ src/components/Header/Favourite.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import heart from "../../assets/heart.svg";
 
 const Favourite = ({ onShow }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsShown(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsShown(false);
+  };
+
   return (
-    <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
-      <img src={heart} alt="heart" />
-      <span onClick={onShow}>Favourite Locations</span>
+    <div
+      className="p-2 cursor-pointer flex gap-2 items-center rounded-md transition-all"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={onShow}
+    >
+      <img
+        src={heart}
+        alt="heart"
+        className={`transition-transform duration-300 ${
+          isShown ? "transform scale-125" : ""
+        }`}
+      />
+      <span
+        className={`transition-opacity duration-300 ${
+          isShown ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        Favourite Locations
+      </span>
     </div>
   );
 };
