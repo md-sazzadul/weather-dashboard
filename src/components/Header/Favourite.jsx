85779aaa5af656ff8fc0b91ed36fe6b17bb28975
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-12-25 21:01:11 +0600

    Revert "added styling and aesthetics"
    This reverts commit dc0be9861ffaa54d9bc8032acf0545593a43449a.

--- src/components/Header/Favourite.jsx
+++ src/components/Header/Favourite.jsx
@@ -1,38 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import heart from "../../assets/heart.svg";
 
 const Favourite = ({ onShow }) => {
-  const [isShown, setIsShown] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsShown(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsShown(false);
-  };
-
   return (
-    <div
-      className="p-2 cursor-pointer flex gap-2 items-center rounded-md transition-all"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onClick={onShow}
-    >
-      <img
-        src={heart}
-        alt="heart"
-        className={`transition-transform duration-300 ${
-          isShown ? "transform scale-125" : ""
-        }`}
-      />
-      <span
-        className={`transition-opacity duration-300 ${
-          isShown ? "opacity-100" : "opacity-0"
-        }`}
-      >
-        Favourite Locations
-      </span>
+    <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
+      <img src={heart} alt="heart" />
+      <span onClick={onShow}>Favourite Locations</span>
     </div>
   );
 };
--- src/components/Header/FavouriteListModal.jsx
+++ src/components/Header/FavouriteListModal.jsx
@@ -1,21 +1,12 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { FavouriteContext, LocationContext } from "../../context";
 
 const FavouriteListModal = () => {
   const { favourites } = useContext(FavouriteContext);
   const { setSelectedLocation } = useContext(LocationContext);
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    setIsVisible(true);
-  }, []);
 
   return (
-    <div
-      className={`max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg transition-all duration-300 ease-in-out ${
-        isVisible ? "animate-fadeIn" : "animate-fadeOut"
-      }`}
-    >
+    <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
       <h3 className="text-lg font-bold px-4">Favourite Locations</h3>
       <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
         {favourites.length > 0 ? (
